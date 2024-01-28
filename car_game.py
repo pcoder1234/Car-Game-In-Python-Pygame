25404517c5f0e2db767de33e8e47e678104bc9a6
pcoder1234 <108308170+pcoder1234@users.noreply.github.com>
2024-01-28 11:00:47 -0500

    v1.0

---
+++ car_game.py
@@ -0,0 +1,114 @@
+import pygame
+import sys
+import random
+import time
+import colorama
+
+# Initializing Pygame
+pygame.init()
+
+# Setting up score
+score = 0
+
+# Setting up screen
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Car game")
+
+screen.fill('#00FF00')
+
+# Set up text
+game_font = pygame.font.SysFont('arial', 16)
+
+# Set up timer
+start_time = time.time()
+elapsed_time = 0
+
+
+
+# Set Up Road
+road_width = 600
+road_color = '#787878'
+road = pygame.Rect(SCREEN_WIDTH/2 - road_width/2, 0, road_width, SCREEN_HEIGHT)
+
+# Set up coin
+coin_height = 25
+coin_width = 25
+coin_speed = 5
+coin_color = '#FFFF00'
+coin = pygame.Rect(SCREEN_WIDTH/2, 0 - coin_height, coin_width, coin_height)
+
+# Set up car
+car = pygame.transform.scale(pygame.transform.rotate(pygame.image.load("car.png"), -90), (50, 100)) # I had to make the car smaller and rotate it 90 degrees to the left
+car_rect = car.get_rect(midbottom=(SCREEN_WIDTH/2, SCREEN_HEIGHT - 50))
+car_speed = 5
+
+# Set Up FPS
+clock = pygame.time.Clock()
+
+running = True
+
+# Game Loop
+while running:
+    # Update timer
+    current_time = time.time()
+    elapsed_time = current_time - start_time
+    time_left = 60 - elapsed_time
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    # Text
+    score_text = game_font.render(f'Score: {score} \n Time: {round(time_left)}', True, 'white', 'orange')
+    gameover_text = game_font.render('Game Over!', True, 'black', 'red')
+    
+    # Render Surfaces
+    pygame.draw.rect(screen, road_color, road)
+    pygame.draw.ellipse(screen, coin_color, coin)
+    screen.blit(car, car_rect)
+    screen.blit(score_text,(SCREEN_HEIGHT/2,10))
+    pygame.display.update()
+
+    # Get User Input w/o car moving offscreen
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and car_rect.x - car_speed >= 100:
+        car_rect.x -= car_speed
+    
+    if keys[pygame.K_RIGHT] and car_rect.x + car_speed - car_rect.width <= 600:
+        car_rect.x += car_speed
+    
+    
+    # Coin Code
+    coin.y += coin_speed
+    if coin.top > SCREEN_HEIGHT:
+        coin.y = 0 - coin_height
+        coin.x = random.randint(100, 600)
+    
+    if coin.colliderect(car_rect):
+        coin.y = 0 - coin_height
+        coin.x = random.randint(100, 600)
+        score += 1
+        if coin_speed + 0.3 < 10:
+            coin_speed += 0.3
+    
+    # Ending the game when out of time
+    if time_left <= 0.1:
+        screen.blit(gameover_text, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+        pygame.time.delay(4000)
+        
+        running = False
+        print('\n'*100)
+        print(f'You got {score} points!')
+    
+    
+    # Setting framerate + updating the display
+    clock.tick(60)
+    pygame.display.flip()
+
+
+
+pygame.quit()
+sys.exit()
